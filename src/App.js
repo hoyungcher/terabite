ccf751b2026e49d1af3f0128d418e911d2ac1d30
Yung Cher Ho <ycho527@hotmail.com>
2020-11-14 00:30:52 +0000

    Added Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import About from './About';
 
 const App = () => {
-  return <Router>
-    <Route path="/"></Route>
-  </Router>;
+  return (
+  <Router>
+    <Switch>
+      <Route path="/">
+        <About />
+      </Route>
+      <Redirect to="/" />
+    </Switch>
+  </Router>
+  );
 }
 
 export default App;
